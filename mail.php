637a0a08a8ffd7d0d9ef01aed3a7005684819e48
Brandon-8829 <Brandonm1997.bh@gmail.com>
2022-06-10 01:39:54 -0600

    commit

---
+++ mail.php
@@ -0,0 +1,10 @@
+<?php
+// the message
+$msg = "First line of text\nSecond line of text";
+
+// use wordwrap() if lines are longer than 70 characters
+$msg = wordwrap($msg,70);
+
+// send email
+mail("Brandonm1997.bh@gmail.com.com","My subject",$msg);
+?>
